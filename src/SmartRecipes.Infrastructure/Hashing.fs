652fee7eaf70923bc3bc597b1d67df1a0475e8cd
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2018-09-08 10:59:51 +0200

    Init

--- src/SmartRecipes.Infrastructure/Hashing.fs
+++
@@ -1,9 +0,0 @@
-module Infrastructure.Hashing
-
-    let hash plain =
-        BCrypt.Net.BCrypt.HashPassword(plain)
-        
-    let verify hashed plain = 
-        BCrypt.Net.BCrypt.Verify(plain, hashed)
-        
-    
--- src/SmartRecipes.Infrastructure/Option.fs
+++
@@ -1,6 +0,0 @@
-module Infrastructure.Option
-
-    let toResult error option = 
-        match option with
-        | Some s -> Ok s 
-        | None -> Error error
